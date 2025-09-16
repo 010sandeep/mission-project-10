60b7a7918b2b104616663e3b61a120f427b6d93a
sandeep gurjar <sandeepgurjardogne202@gmail.com>
2025-09-16 16:24:15 +0530

    16-09-2025

---
+++ ORSProject-10/src/main/java/com/rays/ORSProjectApplication.java
@@ -0,0 +1,14 @@
+package com.rays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ORSProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ORSProjectApplication.class, args);
+		
+	}
+
+}
